9263eca095c950accedbb6ee1dba14a20a7dc1c1
alexdeltufo <alex@ardenwoodsweb.com>
2026-05-25 12:16:41 -0400

    Reset time to midnight for current and weekOut dates

--- api/calendar.js
+++ api/calendar.js
@@ -28,8 +28,8 @@ export default async function handler(req, res) {
     return res.status(401).json({ error: 'NOT_AUTHED' });
   }
 
-  const now = new Date();
-  const weekOut = new Date(now);
+  const now = new Date(new Date().setHours(0,0,0,0));
+  const weekOut = new Date(new Date().setHours(0,0,0,0) + 7*24*60*60*1000);
   weekOut.setDate(now.getDate() + 7);
 
   try {
